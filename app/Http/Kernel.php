0a8cb28b781e862dc6f402b6e89ac5a3b90e0ec4
waheilee <waheilee@gmail.com>
2020-05-29 13:04:57 +0800

    解决api跨域问题

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -21,7 +21,6 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \App\Http\Middleware\AccessControlAllowOrigin::class,
     ];
 
     /**
@@ -38,13 +37,13 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+
         ],
 
         'api' => [
 //            'throttle:60,1',
             'bindings',
             ApiMiddleware::class,
-            'cors'
         ],
     ];
 
@@ -66,7 +65,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'jwtAuth'    => AuthJWT::class,
-        'cors' => \App\Http\Middleware\AccessControlAllowOrigin::class,
     ];
 
     /**
