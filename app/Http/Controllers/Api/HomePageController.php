ec3d73fbafdc1817ab2a00da6843c25c47496a92
waheilee <waheilee@gmail.com>
2020-06-12 17:19:48 +0800

    转态码

--- app/Http/Controllers/Api/HomePageController.php
+++ app/Http/Controllers/Api/HomePageController.php
@@ -12,18 +12,22 @@ class HomePageController extends Controller
 
     public function index()
     {
-        $banner = BannerModel::whereStatus(1)->orderBy('created_at','desc')->get(['title','url','content','type']);
-        $plan = PlanLessonModel::whereStatus(1)->get(['id','title','url']);
-        foreach ($plan as $item){
-            $item->url = env('APP_URL').$item->url;
+        try{
+            $banner = BannerModel::whereStatus(1)->orderBy('created_at','desc')->get(['title','url','content','type']);
+            $plan = PlanLessonModel::whereStatus(1)->get(['id','title','url']);
+            foreach ($plan as $item){
+                $item->url = env('APP_URL').$item->url;
+            }
+            $data = [
+                'indexData' =>[
+                    'banner'=>$banner,
+                    'recommend'=>$plan,
+                ]
+            ];
+            return $this->wrapSuccessReturn(compact('data'));
+        }catch (\Exception $exception){
+            return $this->wrapErrorReturn($exception);
         }
-        $data = [
-            'indexData' =>[
-                'banner'=>$banner,
-                'recommend'=>$plan,
-            ]
-        ];
-        return $data;
 
     }
 }
