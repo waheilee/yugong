22e5669e164a2c32baa202d510ef541226008527
waheilee <waheilee@gmail.com>
2020-06-18 16:59:24 +0800

    添加标签列表

---
+++ database/migrations/2020_06_18_155201_create_nav_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNavTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('exam_nav', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title',50)->comment('标题');
+            $table->string('img_url')->comment('图标地址');
+            $table->integer('status')->comment('状态');
+            $table->text('content')->comment('内容');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('nav');
+    }
+}
