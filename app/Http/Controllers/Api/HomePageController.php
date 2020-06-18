59da0f227d0083f9316d40a3bd5223dfb4e8d03b
waheilee <waheilee@gmail.com>
2020-06-18 14:29:47 +0800

    首页

--- app/Http/Controllers/Api/HomePageController.php
+++ app/Http/Controllers/Api/HomePageController.php
@@ -15,12 +15,6 @@ class HomePageController extends Controller
         try{
             $banner = BannerModel::whereStatus(1)->orderBy('created_at','desc')->get(['title','url','content','type']);
             $plan = PlanLessonModel::whereStatus(1)->limit(3)->get(['id','title','url']);
-//            foreach ($plan as $item){
-//                $item->url = env('APP_URL').$item->url;
-//            }
-            foreach ($banner as $item){
-                $item->url = env('APP_URL').$item->url;
-            }
             $data = [
                 'indexData' =>[
                     'banner'=>$banner,
