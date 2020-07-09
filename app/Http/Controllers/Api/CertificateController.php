7f7d1bfd4e2127364ec45179cc5267998ba31c78
waheilee <waheilee@gmail.com>
2020-07-09 11:34:41 +0800

    证书发放测试

--- app/Http/Controllers/Api/CertificateController.php
+++ app/Http/Controllers/Api/CertificateController.php
@@ -76,6 +76,7 @@ class CertificateController extends Controller
             }
             $require =  json_decode($cerModel->require);
             $record = ExamRecordModel::whereSerUserId($serUserId)->wherePass(1)->get();
+            dd($record);
             $arr = [];//查找出用户通过考试的所有试卷id
             foreach ($record as $item=>$v){
                 $arr[$item] = $v->paper_id;
