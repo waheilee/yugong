468640294fd66a9049adbb94cc80c7887d60d5a8
waheilee <waheilee@gmail.com>
2020-07-27 19:23:18 +0800

    openid

--- app/Http/Controllers/User/WeChatOauthController.php
+++ app/Http/Controllers/User/WeChatOauthController.php
@@ -63,10 +63,10 @@ dd($response);
         Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
 
         $app = app('wechat.official_account');
-        dd($app);
         $app->server->push(function($message){
             return "欢迎关注 overtrue！";
         });
+        dd($app->server->serve());
 
         return $app->server->serve();
     }
