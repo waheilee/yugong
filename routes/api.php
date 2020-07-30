decf261d9591f57c62d9ba232b045e873704db22
waheilee <waheilee@gmail.com>
2020-07-30 17:40:05 +0800

    支付接口

--- routes/api.php
+++ routes/api.php
@@ -123,12 +123,11 @@ Route::middleware('cors')->group(function () {
     Route::post('serversList', 'User\ServersController@serversList');
     //服务产品详情
     Route::post('serversDetail', 'User\ServersController@serversDetail');
-    //确认订单
-    Route::post('confirmOrder','User\ServersController@createdOrder');
+
     //确认支付
-    Route::post('wechat/pay','User\ServersController@weChatPay');
+    Route::post('wechat/pay','User\WeChatController@weChatPay');
     //微信回调接口
-    Route::get('wechat/pay/notify','User\ServersController@notify');
+    Route::get('wechat/pay/notify','User\WeChatController@notify');
 
     Route::group(['middleware' => ['jwtUserAuth']],function(){
         //用户详情接口
@@ -137,7 +136,8 @@ Route::middleware('cors')->group(function () {
         Route::post('userLogout', 'User\UserController@logout');
         //订单详情接口
         Route::post('orderDetail','User\ServersController@orderDetail');
-
+        //确认订单
+        Route::post('confirmOrder','User\ServersController@createdOrder');
 
         Route::post('createOrder','User\ServersController@createdOrder');
 
